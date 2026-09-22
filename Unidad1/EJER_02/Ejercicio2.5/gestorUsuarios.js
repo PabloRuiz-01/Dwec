9d92c431dab82e89315ace0116eb8134b64ba4a5
Pablo <XTP82531@educastur.es>
2026-09-22 14:26:07 +0200

    Avanzado ejercicio 2.5

--- Unidad1/EJER_02/Ejercicio2.5/gestorUsuarios.js
+++ Unidad1/EJER_02/Ejercicio2.5/gestorUsuarios.js
@@ -15,3 +15,25 @@ export function mostrarPerfil(usuario) {
     console.log(`Nombre: ${usuario.nombre}, Email: ${usuario.email}, Edad: ${usuario.edad}`)
 
 }
+
+//Añade y exporta una nueva función esMayorDeEdad
+
+export function esMayorDeEdad(usuario) {
+    
+    if (usuario.edad>18) {
+        return true
+    }else{
+        return false
+    }
+}
+
+//Añade y exporta una función obtenerMayoresDeEdad que reciba un array de usuarios y, utilizando el método .filter() y la función 
+// esMayorDeEdad, devuelva un nuevo array solo con los usuarios que cumplen la condición.
+
+export function obtenerMayoresDeEdad(usuario) {
+    
+    return usuarios.filter(usuario => esMayorDeEdad)
+}
+
+//Añade y exporta una función calcularPromedioEdad que reciba un array de usuarios y calcule su edad 
+//promedio. Puedes usar el método .reduce() para esto.
--- Unidad1/EJER_02/Ejercicio2.5/main.js
+++ Unidad1/EJER_02/Ejercicio2.5/main.js
@@ -1,3 +1,17 @@
 //Importa crearPerfil y mostrarPerfil desde gestorUsuarios.js
-import {crearPerfil,mostrarPerfil } from "gestorUsuarios";
+import {crearPerfil,mostrarPerfil,esMayorDeEdad,obtenerMayoresDeEdad } from "./gestorUsuarios.js";
+
+//Llama a crearPerfil dos veces para crear dos perfiles de usuario y guárdalos en un array .
+
+const usuarios=[
+crearPerfil("Pablo","emailejemplo@educastur.es",25),
+crearPerfil("Sergio","emailejemplo2@educastur.es",19)]
+
+//Itera sobre el array usuarios y, para cada usuario, utiliza mostrarPerfil para imprimir su información en la consola.
+
+usuarios.forEach(usuario => {
+   mostrarPerfil(usuario)
+});
+
+
 
